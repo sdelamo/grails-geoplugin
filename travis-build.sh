b073869612504e20add8b74a25bb2bf83f4767aa
Sergio del Amo <sergio.delamo@softamo.com>
2016-10-09 17:17:27 +0200

    Fix build script

--- travis-build.sh
+++ travis-build.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-./gradlew clean check assemble --stacktrace
+./gradlew clean check
 
 EXIT_STATUS=0
-echo "Publishing archives for branch $TRAVIS_BRANCH"
-if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-  if [[ -n $TRAVIS_TAG ]]; then
-    echo "Pushing build to Bintray"
-    ./gradlew bintrayUpload || EXIT_STATUS=$?
-  else
-    echo "Publishing snapshot to OJO"
-    ./gradlew artifactoryPublish || EXIT_STATUS=$?
-  fi
-fi
+
 exit $EXIT_STATUS
 fi
